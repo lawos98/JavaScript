b797537f8b2d071d238c9b4f1ed69eb1092e7c63
lawos98 <85324038+lawos98@users.noreply.github.com>
2022-05-05 03:21:04 +0200

    Update lab_5

---
+++ lab_5/toLab/zad_2/test/test.js
@@ -0,0 +1,72 @@
+const supertest = require('supertest');
+
+const chai = require('chai');
+const { expect } = require('chai');
+chai.use(require('chai-json'));
+
+const { getHtmlResponse, calculated, eval } = require('../script');
+
+describe('Check json', () => {
+    it('Is it json file', () => {
+        expect('./json/test.json')
+            .to.be.a.jsonObj()
+            .and.contain.jsonWithProps({ x: 2, y: 2, operation: '+' });
+    });
+});
+
+describe('Check eq', () => {
+    it('Returns 4 for 2 + 2', () => {
+        expect(eval('+',2, 2)).to.be.equal(4);
+    });
+    it('Returns 0 for 2 - 2', () => {
+        expect(eval('-', 2, 2)).to.be.equal(0);
+    });
+    it('Returns 5 for 1 * 5', () => {
+        expect(eval('*', 1, 5)).to.be.equal(5);
+    });
+    it('Returns 0 for 0 * 4', () => {
+        expect(eval('*', 0, 4)).to.be.equal(0);
+    });
+    it('Returns Infinity for 10 / 2', () => {
+        expect(eval('/', 10, 2)).to.be.equal(5);
+    });
+});
+
+describe('Check HTML', () => {
+    it('html for 4 + 7 =11 ', () => {
+        const json = [{ operation: '+', x: 4, y: 7 }];
+        expect(getHtmlResponse(json))
+            .to.include('<table>')
+            .and.to.include('<tr>')
+            .and.to.include('<td>')
+            .and.to.include('<th>')
+            .and.to.include('11');
+    });
+    it('html for 3 * 6 = 18 ', () => {
+        const json = [{ operation: '*', x: 3, y: 6 }];
+        expect(getHtmlResponse(json))
+            .to.include('<table>')
+            .and.to.include('<tr>')
+            .and.to.include('<td>')
+            .and.to.include('<th>')
+            .and.to.include('18');
+    });
+    it('html for 2 - -6 = 8 ', () => {
+        const json = [{ operation: '-', x: 2, y: -6 }];
+        expect(getHtmlResponse(json))
+            .to.include('<table>')
+            .and.to.include('<tr>')
+            .and.to.include('<td>')
+            .and.to.include('<th>')
+            .and.to.include('8');
+    });
+    it('html for 5 / -1 = -5 ', () => {
+        const json = [{ operation: '/', x: 5, y: -1 }];
+        expect(getHtmlResponse(json))
+            .to.include('<table>')
+            .and.to.include('<tr>')
+            .and.to.include('<td>')
+            .and.to.include('<th>')
+            .and.to.include('-5');
+    });
+});
